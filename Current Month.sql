eda26d961dd28c167a6f49c92a7cb9a1213b4b2b
yashbhutoria <yash222219@gmail.com>
2018-05-05 13:00:15 +0530

    uploaded schema for two tables

---
+++ Current Month.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [Current Month] (
+    [Time] datetime,
+    [Vertical Subset Except Default] varchar(50),
+    [Current Headcount] int,
+    [AOP Vacant Positions] int,
+    [TA Open] int,
+    [Attrition %] int,
+    [Attrition Backfills] int,
+    [Sub Grp Vertical] int
+)
