d3592cbd46dc22936507981cfa25fa3c01048265
yashbhutoria <yash222219@gmail.com>
2018-05-08 00:22:31 +0530

    changed query to work with CSV files

--- CurrencyConversionRate2018.sql
+++ CurrencyConversionRate2018.sql
@@ -1,23 +1,13 @@
-drop table CurrencyConversionRate2018;
 CREATE TABLE CurrencyConversionRate2018(
 [Currency] VARCHAR(10),
 [AOP FY19] FLOAT,
 );
 
-EXEC sp_configure 'show advanced options', 1
-RECONFIGURE
-GO
-EXEC sp_configure 'ad hoc distributed queries', 1
-RECONFIGURE
-GO
-
-EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'AllowInProcess' , 1
-GO
-EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'DynamicParameters' , 1
-GO
-
-
-SELECT * into [CurrencyConversionRate2018]
-FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
-                'Excel 12.0;Database=D:\binset\CurrencyConversionRate2018.xlsx;IMEX=1',
-                'SELECT * FROM [Sheet1$]');
+BULK INSERT [dbo].[CurrencyConversionRate2018]
+FROM 'D:\binset\CurrencyConversionRate2018.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR= ',',
+ROWTERMINATOR = '\n'
+)
--- mapVetricaltoBU.sql
+++ mapVetricaltoBU.sql
@@ -1,46 +1,38 @@
+drop table mapVetricaltoBU
 CREATE TABLE mapVetricaltoBU(
- [insert_unit] NVARCHAR(30),
+ [insert_unit] NVARCHAR(max),
  [Parent] NVARCHAR(100), 
  [Write] NVARCHAR(50), 
- [Read] NVARCHAR(30), 
+ [Read] NVARCHAR(100), 
  [Code] NVARCHAR(100), 
  [Status] NVARCHAR(100), 
- [Vertical Subset Except Default] NVARCHAR(30), 
- [Vertical Subset] NVARCHAR(30), 
+ [Vertical Subset Except Default] NVARCHAR(100), 
+ [Vertical Subset] NVARCHAR(100), 
  [SS_DefaultVertical] NVARCHAR(100), 
- [Description] NVARCHAR(30), 
- [VERTICAL Status] NVARCHAR(30), 
- [Vertical] NVARCHAR(30), 
- [LOB Link] NVARCHAR(30), 
+ [Description] NVARCHAR(max), 
+ [VERTICAL Status] NVARCHAR(100), 
+ [Vertical] NVARCHAR(max), 
+ [LOB Link] NVARCHAR(max), 
  [BU] NVARCHAR(100), 
  [User-Matrix BU] NVARCHAR(100), 
- [User-Matrix LOB] NVARCHAR(30), 
+ [User-Matrix LOB] NVARCHAR(100), 
  [User-Matrix Vertical] NVARCHAR(max), 
- [LOB] NVARCHAR(30), 
+ [LOB] NVARCHAR(max), 
  [Default Vertical BU NH] NVARCHAR(100), 
  [Default Vertical LOB NH] NVARCHAR(100), 
- [Group Hierarchy] NVARCHAR(30), 
+ [Group Hierarchy] NVARCHAR(100), 
  [BU Group Hierarchy] NVARCHAR(30), 
- [Vertical - B3] NVARCHAR(30), 
+ [Vertical - B3] NVARCHAR(max), 
  [xparent-code] NVARCHAR(100), 
  [Test] NVARCHAR(max)
 );
 
 
-EXEC sp_configure 'show advanced options', 1
-RECONFIGURE
-GO
-EXEC sp_configure 'ad hoc distributed queries', 1
-RECONFIGURE
-GO
-
-EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'AllowInProcess' , 1
-GO
-EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'DynamicParameters' , 1
-GO
-
-
-SELECT * into [CurrencyConversionRate2018]
-FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
-                'Excel 12.0;Database=D:\binset\mapVetricaltoBU.xls;IMEX=1',
-                'SELECT * FROM [Sheet1$]');
+BULK INSERT [dbo].[mapVetricaltoBU]
+FROM 'D:\binset\mapVetricaltoBU.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR= ',',
+ROWTERMINATOR = '\n'
+)
